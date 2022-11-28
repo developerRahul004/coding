c56389607440525431d769307743b023e27666e8
Rahul Mishra <47867953+developerRahul004@users.noreply.github.com>
2022-11-28 23:24:00 +0530

    Two Sum
    https://leetcode.com/problemset/all/?sorting=W3sic29ydE9yZGVyIjoiQVNDRU5ESU5HIiwib3JkZXJCeSI6IkZST05URU5EX0lEIn1d

---
+++ codebase/all/src/com/leetcode/math/easy/TwoSum.java
@@ -0,0 +1,36 @@
+package com.leetcode.math.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+	public static void main(String[] args) {
+		
+		TwoSum twoSum = new TwoSum();
+		
+		int[] input = {2,7,11,15};
+		int target = 9;
+		
+		int[] result = twoSum.twoSum(input, target);
+		for(int i = 0 ; i < result.length ; i++) {
+			System.out.println(result[i]);
+		}
+	}
+
+
+	public int[] twoSum(int[] nums, int target) {
+
+		Map<Integer, Integer> map = new HashMap<>();
+
+		for (int i = 0; i < nums.length; i++) {
+			int val = target - nums[i];
+			if (map.containsKey(val)) {
+				return new int[] { map.get(val), i };
+			}
+			map.put(nums[i], i);
+		}
+		return null;
+	}
+
+}
